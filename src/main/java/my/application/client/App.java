63d0ef83905f93373e918d35ac442349030f563f
Igor M <zzzsmiller@mail.ru>
2012-07-20 19:07:49 +0400

    init project

---
+++ src/main/java/my/application/client/App.java
@@ -0,0 +1,31 @@
+package my.application.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class App implements EntryPoint {
+  /**
+   * The message displayed to the user when the server cannot be reached or
+   * returns an error.
+   */
+  private static final String SERVER_ERROR = "An error occurred while "
+      + "attempting to contact the server. Please check your network "
+      + "connection and try again.";
+
+  /**
+   * Create a remote service proxy to talk to the server-side Greeting service.
+   */
+  private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+
+  private final Messages messages = GWT.create(Messages.class);
+
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad() {
+
+  }
+}
