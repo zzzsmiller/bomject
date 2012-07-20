8364ae192f37b7850c1b59de9642c27654fadbad
Igor M <zzzsmiller@mail.ru>
2012-07-20 19:09:31 +0400

    init project

---
+++ target/.generated/my/application/client/GreetingService_Proxy.java
@@ -0,0 +1,52 @@
+package my.application.client;
+
+import com.google.gwt.user.client.rpc.impl.RemoteServiceProxy;
+import com.google.gwt.user.client.rpc.impl.ClientSerializationStreamWriter;
+import com.google.gwt.user.client.rpc.SerializationStreamWriter;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.impl.RequestCallbackAdapter.ResponseReader;
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.client.rpc.RpcToken;
+import com.google.gwt.user.client.rpc.RpcTokenException;
+import com.google.gwt.core.client.impl.Impl;
+import com.google.gwt.user.client.rpc.impl.RpcStatsContext;
+
+public class GreetingService_Proxy extends RemoteServiceProxy implements my.application.client.GreetingServiceAsync {
+  private static final String REMOTE_SERVICE_INTERFACE_NAME = "my.application.client.GreetingService";
+  private static final String SERIALIZATION_POLICY ="EB8A9D5B55C137E19694289D1B381D5E";
+  private static final my.application.client.GreetingService_TypeSerializer SERIALIZER = new my.application.client.GreetingService_TypeSerializer();
+  
+  public GreetingService_Proxy() {
+    super(GWT.getModuleBaseURL(),
+      "greet", 
+      SERIALIZATION_POLICY, 
+      SERIALIZER);
+  }
+  
+  public void greetServer(java.lang.String name, com.google.gwt.user.client.rpc.AsyncCallback callback) {
+    com.google.gwt.user.client.rpc.impl.RemoteServiceProxy.ServiceHelper helper = new com.google.gwt.user.client.rpc.impl.RemoteServiceProxy.ServiceHelper("GreetingService_Proxy", "greetServer");
+    try {
+      SerializationStreamWriter streamWriter = helper.start(REMOTE_SERVICE_INTERFACE_NAME, 1);
+      streamWriter.writeString("java.lang.String/2004016611");
+      streamWriter.writeString(name);
+      helper.finish(callback, ResponseReader.STRING);
+    } catch (SerializationException ex) {
+      callback.onFailure(ex);
+    }
+  }
+  @Override
+  public SerializationStreamWriter createStreamWriter() {
+    ClientSerializationStreamWriter toReturn =
+      (ClientSerializationStreamWriter) super.createStreamWriter();
+    if (getRpcToken() != null) {
+      toReturn.addFlags(ClientSerializationStreamWriter.FLAG_RPC_TOKEN_INCLUDED);
+    }
+    return toReturn;
+  }
+  @Override
+  protected void checkRpcTokenType(RpcToken token) {
+    if (!(token instanceof com.google.gwt.user.client.rpc.XsrfToken)) {
+      throw new RpcTokenException("Invalid RpcToken type: expected 'com.google.gwt.user.client.rpc.XsrfToken' but got '" + token.getClass() + "'");
+    }
+  }
+}
---
+++ target/.generated/my/application/client/Messages_.java
@@ -0,0 +1,12 @@
+package my.application.client;
+
+public class Messages_ implements my.application.client.Messages {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
