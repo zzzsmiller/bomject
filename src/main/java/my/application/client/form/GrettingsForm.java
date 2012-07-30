b9ffbcc4e1a0e3bcdc84f17fe8c591adb9c826c5
Igor M <zzzsmiller@mail.ru>
2012-07-30 11:32:11 +0400

    dell unnecessary

--- src/main/java/my/application/client/form/GrettingsForm.java
+++ src/main/java/my/application/client/form/GrettingsForm.java
@@ -2,8 +2,6 @@ package my.application.client.form;
 
 import com.extjs.gxt.ui.client.widget.TabItem;
 import com.extjs.gxt.ui.client.widget.TabPanel;
-import com.google.gwt.event.logical.shared.ResizeEvent;
-import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.ui.HTML;
 
 
@@ -16,30 +14,15 @@ import com.google.gwt.user.client.ui.HTML;
  */
 public class GrettingsForm extends TabPanel {
 
-    /*public GrettingsForm() {
-        this(2.5, EM);
-    }*/
-//    private GrettingsForm instance;
-
     public GrettingsForm() {
 
         initTabBar();
 
         setAnimScroll(true);
         setBorders(true);
-//        TabPanelMessages message = new TabPanelMessages();
-//        message.setCloseText("Некое сообщение");
-//        setMessages(message);
         setDeferHeight(false);
         setHeight("200px");
 
-        /*getParent().addHandler(new ResizeHandler() {
-            @Override
-            public void onResize(ResizeEvent event) {
-                setHeight(FormSizeUtil.pixelFormat(event.getHeight()));
-                setWidth(FormSizeUtil.pixelFormat(event.getWidth()));
-            }
-        }, ResizeEvent.getType());*/
     }
 
     private void initTabBar() {
@@ -64,31 +47,6 @@ public class GrettingsForm extends TabPanel {
         tabItem.addText("ble-ble-ble");
         add(tabItem);
 
-        /*add(new HTML("bla-bla-bla"), "Просмотр всех комментариев");
-        add(new HTML("blu-blu-blu"), "Добавить комментарий");
-        add(new HTML("bli-bli-bli"), "Редактировать комментарий");
-        add(new HTML("ble-ble-ble"), "Удалить комментарий");
-*/
-
     }
-
-    @Override
-    protected void onAttach() {
-        super.onAttach();
-//        setSize(getParent().getOffsetWidth(), getParent().getOffsetHeight());
-        getParent().addHandler(new ResizeHandler() {
-            @Override
-            public void onResize(ResizeEvent event) {
-                setSize(event.getWidth(), event.getHeight());
-            }
-        }, ResizeEvent.getType());
-    }
-
-    /*@Override
-    protected void onResize(int width, int height) {
-        super.onResize(width, height);
-        setSize(getParent().getOffsetWidth(), getParent().getOffsetHeight());
-    }*/
-
 
 }
